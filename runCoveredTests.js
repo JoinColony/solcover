eb237626ebb47248044efe8ca49fe6e0d6f93831
Alex Rea <git@area.io>
2017-01-04 20:22:33 +0000

    Allow nested contract directories

--- runCoveredTests.js
+++ runCoveredTests.js
@@ -7,6 +7,7 @@ var coverage = {};
 var fs = require('fs');
 var path = require('path');
 var getInstrumentedVersion = require('./instrumentSolidity.js');
+var mkdirp = require('mkdirp');
 
 var childprocess = require('child_process');
 
@@ -26,14 +27,15 @@ if (shell.test('-d','../originalContracts')){
 shell.mv('./../contracts/', './../originalContracts/');
 shell.mkdir('./../contracts/');
 //For each contract in originalContracts, get the instrumented version
-shell.ls('./../originalContracts/*.sol').forEach(function(file) {
+shell.ls('./../originalContracts/**/*.sol').forEach(function(file) {
     if (file !== 'originalContracts/Migrations.sol') {
         console.log("instrumenting ", file);
         var contract = fs.readFileSync("./" + file).toString();
         var fileName = path.basename(file);
-        var instrumentedContractInfo = getInstrumentedVersion(contract, fileName, true);
-        fs.writeFileSync('./../contracts/' + path.basename(file), instrumentedContractInfo.contract);
-        var canonicalContractPath = path.resolve('./../originalContracts/' + path.basename(file));
+        var instrumentedContractInfo = getInstrumentedVersion(contract, file, true);
+        mkdirp.sync(path.dirname(file.replace('originalContracts', 'contracts')));
+        fs.writeFileSync(file.replace('originalContracts','contracts'), instrumentedContractInfo.contract);
+        var canonicalContractPath = path.resolve(file);
         coverage[canonicalContractPath] = { "l": {}, "path": canonicalContractPath, "s": {}, "b": {}, "f": {}, "fnMap": {}, "statementMap": {}, "branchMap": {} };
         for (idx in instrumentedContractInfo.runnableLines) {
             coverage[canonicalContractPath]["l"][instrumentedContractInfo.runnableLines[idx]] = 0;
@@ -64,19 +66,19 @@ for (idx=0; idx < events.length-1; idx++){
     var event = JSON.parse(events[idx]);
     if (event.topics.indexOf("b8995a65f405d9756b41a334f38d8ff0c93c4934e170d3c1429c3e7ca101014d") >= 0) {
         var data = SolidityCoder.decodeParams(["string", "uint256"], event.data.replace("0x", ""));
-        var canonicalContractPath = path.resolve('./../originalContracts/' + path.basename(data[0]));
+        var canonicalContractPath = path.resolve(data[0]);
         coverage[canonicalContractPath]["l"][data[1].toNumber()] += 1;
     }else if(event.topics.indexOf("d4ce765fd23c5cc3660249353d61ecd18ca60549dd62cb9ca350a4244de7b87f")>=0){
         var data = SolidityCoder.decodeParams(["string", "uint256"], event.data.replace("0x", ""));
-        var canonicalContractPath = path.resolve('./../originalContracts/' + path.basename(data[0]));
+        var canonicalContractPath = path.resolve(data[0]);
         coverage[canonicalContractPath]["f"][data[1].toNumber()] += 1;
     }else if(event.topics.indexOf("d4cf56ed5ba572684f02f889f12ac42d9583c8e3097802060e949bfbb3c1bff5")>=0){
         var data = SolidityCoder.decodeParams(["string", "uint256", "uint256"], event.data.replace("0x", ""));
-        var canonicalContractPath = path.resolve('./../originalContracts/' + path.basename(data[0]));
+        var canonicalContractPath = path.resolve(data[0]);
         coverage[canonicalContractPath]["b"][data[1].toNumber()][data[2].toNumber()] += 1;
     }else if(event.topics.indexOf("b51abbff580b3a34bbc725f2dc6f736e9d4b45a41293fd0084ad865a31fde0c8")>=0){
         var data = SolidityCoder.decodeParams(["string","uint256"], event.data.replace("0x", ""));
-        var canonicalContractPath = path.resolve('./../originalContracts/' + path.basename(data[0]));
+        var canonicalContractPath = path.resolve(data[0]);
         coverage[canonicalContractPath]["s"][data[1].toNumber()]+= 1;
     }
 }
